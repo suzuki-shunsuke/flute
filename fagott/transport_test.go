0b241b1a6b8806399f427bb092b908ec9f7b4333
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-07-07 06:56:02 +0900

    test: add tests

--- fagott/transport_test.go
+++ fagott/transport_test.go
@@ -1,6 +1,7 @@
 package fagott
 
 import (
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -14,11 +15,13 @@ import (
 func TestTransport_RoundTrip(t *testing.T) {
 	token := "XXXXX"
 	data := []struct {
-		title     string
-		req       *http.Request
-		transport *Transport
-		isErr     bool
-		exp       *http.Response
+		title            string
+		req              *http.Request
+		transport        *Transport
+		isErr            bool
+		defaultClient    *http.Client
+		defaultTransport http.RoundTripper
+		exp              *http.Response
 	}{
 		{
 			title: "normal",
@@ -35,10 +38,19 @@ func TestTransport_RoundTrip(t *testing.T) {
 				},
 			},
 			transport: &Transport{
+				T: t,
 				Services: []Service{
+					{
+						Endpoint: "http://example.org",
+					},
 					{
 						Endpoint: "http://example.com",
 						Routes: []Route{
+							{
+								Matcher: &Matcher{
+									Method: "GET",
+								},
+							},
 							{
 								Name: "create a user",
 								Matcher: &Matcher{
@@ -74,6 +86,38 @@ func TestTransport_RoundTrip(t *testing.T) {
 			},
 		},
 		{
+			title: "failed to match",
+			req: &http.Request{
+				URL: &url.URL{
+					Scheme: "http",
+					Host:   "example.com",
+					Path:   "/users",
+				},
+				Method: "POST",
+				Body:   ioutil.NopCloser(strings.NewReader(`{"name": "foo", "email": "foo@example.com"}`)),
+				Header: http.Header{
+					"Authorization": []string{"token " + token},
+				},
+			},
+			transport: &Transport{
+				Services: []Service{
+					{
+						Endpoint: "http://example.com",
+						Routes: []Route{
+							{
+								Matcher: &Matcher{
+									Match: func(req *http.Request) (bool, error) {
+										return false, errors.New("failed to match")
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			isErr: true,
+		},
+		{
 			title: "transport.Transport is called",
 			req:   &http.Request{},
 			transport: &Transport{
@@ -86,10 +130,60 @@ func TestTransport_RoundTrip(t *testing.T) {
 				StatusCode: 401,
 			},
 		},
+		{
+			title: "http.DefaultClient is used",
+			req: &http.Request{
+				URL: &url.URL{
+					Scheme: "http",
+					Host:   "example.com",
+				},
+			},
+			transport: &Transport{},
+			defaultClient: &http.Client{
+				Transport: NewMockRoundTripper(t, gomic.DoNothing).
+					SetReturnRoundTrip(&http.Response{
+						StatusCode: 401,
+					}, nil),
+			},
+			exp: &http.Response{
+				StatusCode: 401,
+			},
+		},
+		{
+			title: "http.DefaultTransport is used",
+			req: &http.Request{
+				URL: &url.URL{
+					Scheme: "http",
+					Host:   "example.com",
+				},
+			},
+			transport: &Transport{},
+			defaultTransport: NewMockRoundTripper(t, gomic.DoNothing).
+				SetReturnRoundTrip(&http.Response{
+					StatusCode: 401,
+				}, nil),
+			exp: &http.Response{
+				StatusCode: 401,
+			},
+		},
 	}
 
 	for _, d := range data {
 		t.Run(d.title, func(t *testing.T) {
+			if d.defaultClient != nil {
+				defer func(h *http.Client) {
+					http.DefaultClient = h
+				}(http.DefaultClient)
+				http.DefaultClient = d.defaultClient
+			}
+			if d.defaultTransport != nil {
+				defer func(c, r http.RoundTripper) {
+					http.DefaultClient.Transport = c
+					http.DefaultTransport = r
+				}(http.DefaultClient.Transport, http.DefaultTransport)
+				http.DefaultClient.Transport = d.transport
+				http.DefaultTransport = d.defaultTransport
+			}
 			resp, err := d.transport.RoundTrip(d.req)
 			if d.isErr {
 				require.NotNil(t, err)
