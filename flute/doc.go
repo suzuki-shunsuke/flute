435041a0135f3ee2e0f9d9b288fd6a66aa3db547
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-07-17 10:05:11 +0900

    docs: add the description to the GoDoc

---
+++ flute/doc.go
@@ -0,0 +1,11 @@
+/*
+Package flute provides the HTTP client testing framework.
+With flute you can define test cases declaratively.
+You can test request parameters such as the request path, headers, and body.  And you can also mock the HTTP server.
+
+flute uses https://github.com/stretchr/testify internally,
+so it is easy to understand why the test is failed.
+
+Please see the examples https://github.com/suzuki-shunsuke/flute/tree/master/examples .
+*/
+package flute
