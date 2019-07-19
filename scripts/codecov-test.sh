d54441283ab2713aa3449af313c2629020acf031
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-07-20 07:08:32 +0900

    ci: test the example package but ignore the coverage

--- scripts/codecov-test.sh
+++ scripts/codecov-test.sh
@@ -3,7 +3,9 @@
 
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
+# the example package is tested but the coverage is ignored.
+go test -race -covermode=atomic ./examples || exit 1
+for d in $(go list ./... | grep -v vendor | grep -v examples); do
   go test -race -coverprofile=profile.out -covermode=atomic $d || exit 1
   if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
